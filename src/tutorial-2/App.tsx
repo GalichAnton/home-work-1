c13b3027f84197f165bfd9d16cb6ee6e0330b2a8
Anton Galich <inthesky133@gmail.com>
2021-12-13 13:41:54 +0300

    Урок №11. Создать форму авторизации

---
+++ src/tutorial-2/App.tsx
@@ -0,0 +1,14 @@
+import '../index.css'
+import Form from './components/Form'
+
+
+const App = () => {
+
+  return (
+    <div className='app'>
+      <Form className='form'/>
+    </div>
+  )
+}
+
+export default App
