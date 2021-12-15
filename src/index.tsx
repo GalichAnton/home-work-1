f59864d96e24565b953ecaa2189ee72de0c39c98
Anton Galich <inthesky133@gmail.com>
2021-12-15 12:51:30 +0300

    ДЗ №6. Сделать вывод полной записи по отдельному пути

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import './index.css';
 //import App from './tutorial-2/App'
 //import App from './tutorial-3/App'
 //import App from './tutorial-4/App'
-import App from './tutorial-5/App'
+import App from './tutorial-6/App'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
